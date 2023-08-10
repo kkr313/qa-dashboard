4394eba343aedbd9548b64be6e5fd9b70c0203fe
Karan <kkr31398@gmail.com>
2023-08-11 00:04:22 +0530

    test

--- demo/service/AutomationService.js
+++ demo/service/AutomationService.js
@@ -1,8 +1,7 @@
 export const AutomationService = {
     async getAutomationResult() {
-      // const response = await fetch('http://localhost:3000/api/automationReport');
-      const response = await fetch('https://qadash.netlify.app/api/automationReport');
-      const data = await response.json();
-      return data;
+        const response = await fetch('https://cqrs.freightbro.in/automationReport');
+        const data = await response.json();
+        return data;
       }
 }
