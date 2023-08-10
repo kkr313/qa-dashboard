85bab9fb4e7cb900457108d25e507958c6afc6ac
Karan <kkr31398@gmail.com>
2023-08-11 00:00:44 +0530

    testing

--- api-server.js
+++ api-server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 // const PORT = 3000;
-const PORT = 'https://qadash.netlify.app';
+const PORT = 443;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
